df2a87b728a5f3dbb4282d4001d13f55933f6549
Pedro Lucas <pedrolucas.7@hotmail.com>
2022-05-03 20:02:40 -0300

    Adicionado validaçoes e tratamento de erros

--- src/views/register/Form.vue
+++ src/views/register/Form.vue
@@ -1,6 +1,6 @@
 <template>
-  <dir id="user-form">
-    <form action="" class="w-50" id="formulario" @submit="checkForm">
+  <!-- <dir id="user-form"> -->
+    <form action="" class="col-md-8" id="formulario" @submit="checkForm">
       <h3 class="text-center pb-4"><strong>Cadastrar usuário</strong></h3>
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Seu nome</label>
@@ -12,23 +12,18 @@
       </div>
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Senha</label>
-        <input type="password" class="form-control" v-model="usuario.password" required>
+        <input type="password" class="form-control" v-model="usuario.senha" required>
       </div>
+      <label for="exampleInputEmail1" class="form-label">Confirmar senha</label>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Confirmar senha</label>
-        <input type="password" class="form-control" v-model="usuario.confirmPassword" required>
+        <input type="password" class="form-control" v-model="usuario.confirmarSenha" required>
       </div>
-      <button type="submit" class="btn btn-success" @click="cadastrar">Cadastrar</button>
-      <p v-if="errors.length" class="mt-2">
-      <b>Por favor, corrija o(s) seguinte(s) erro(s):</b>
-      <ul>
-        <li v-for="error in errors" :key="error" class="text-danger" style="list-style: none;">{{ error }}</li>
-      </ul>
-      </p>
+      <div class="col-md-12">
+        <button type="submit" class="btn btn-success" :disabled="desabilitar()" @click="cadastrar">Cadastrar</button>
+      </div>
     </form>
-  </dir>
+  <!-- </dir> -->
 </template>
-
 
 <script>
 import api from "../../services/api";
@@ -36,58 +31,39 @@ export default {
   name: "user-form",
   data(){
     return{
-      errors: [],
       usuario: {
         nome: '',
         email: '',
-        password: '',
-        confirmPassword: '',
+        senha: '',
+        confirmarSenha: '',
         dataHoraCadastro: Date.now(),
-        status: 'a',
+        status: 1,
         perfilPermissao: 1
       }
     }
   },
   methods: {
-    checkForm: function (e) {
-      if (this.usuario.nome) {
-        return true;
-      }
-
-      this.errors = [];
-
-      if (!this.usuario.nome) {
-        this.errors.push('O nome é obrigatório.');
-      }
-
-      if (!this.usuario.email) {
-        this.errors.push('O email é obrigatório.');
-      }
-      
-      if (!this.usuario.password) {
-        this.errors.push('A senha é obrigatório.');
-      }
-
-      if(!this.usuario.confirmPassword){
-        this.errors.push('A confirmação da senha é obrigatória');
-      }
-
-      if(this.usuario.password != this.usuario.confirmPassword){
-        this.errors.push(e.error.message);
-      }
-
-      e.preventDefault();
-    },
     cadastrar(){
-      if(this.password === this.confirmPassword){
-        api.post("/usuario", this.usuario).then((r) => {
-        console.log(this.usuario, r)
-      })
+      if (this.usuario.senha != this.usuario.confirmarSenha){
+        alert('A senha não confere com a confirmaçao');
+      } else {
+        api.post("/usuario/criar-usuario", this.usuario)
+        .then((r) => {
+            alert('Usuário criado com sucesso');
+            window.location.reload();
+        }).catch(error => {
+            alert('Erro ao criar usuário ' + error.response.data.message);
+        })
       }
+    },
+    desabilitar(){
+      console.log(this.usuario.nome + ' xx ' + this.usuario.email + ' xx ' + this.usuario.senha + ' xx ' + this.usuario.confirmarSenha);
+      return (!this.usuario.nome || this.usuario.nome > 100) ||
+             (!this.usuario.email || this.usuario.email > 150) ||
+             (!this.usuario.senha || this.usuario.senha > 100) ||
+             (!this.usuario.confirmarSenha || this.usuario.confirmarSenha > 100);
     }
   },
-  
-  
 }
 </script>
 
