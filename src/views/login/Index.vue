aa2a211916886f768cec7fdad47f87da5aebd5dc
Pedro Lucas <pedrolucas.7@hotmail.com>
2022-05-12 00:12:55 -0300

    Alteração no layout nas telas de login e cadastro de usuário e refatorado metodo de cadastrar usuário

--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -1,25 +1,22 @@
 <template>
-  <div class="col-md-12">
-    <div class="container">
-      <!--<form class="form-group">-->
-        <h2>Bem vindo!</h2>
-          <label for="email" class="form-label">Email</label>
-          <div class="col-md-6">
-            <input type="email" name="email" class="form-control" v-model="usuario.email" required>
+  <div class="login-page">
+    <div class="form">
+      <form class="login">
+        <div class="form-group">
+          <div class="label">
+            <label for="email">Email</label>
           </div>
-          <br>
-          <label for="senha" class="form-label">Senha</label>
-          <div class="col-md-6">
-            <input type="password" name="senha" class="form-control" v-model="usuario.senha" required>
-          </div>
-          <span v-if="error" class="alert alert-warning" for="">{{error}}</span>
-          <div>
-            <button @click="logar()" type="submit" class="btn btn-success">Entrar</button>
-          </div>
-          <div class="text-center mt-5">
-          <router-link to="/usuario-register">Não possui um cadastro ? Clique aqui</router-link>
+          <input id="email" type="email" class="form-control" placeholder="exemplo@exemplo.com" v-model="usuario.email" required>
         </div>
-     <!-- </form>-->
+        <div class="form-group">
+          <div class="label">
+            <label for="senha">Senha</label>  
+          </div>
+          <input id="senha" type="password" class="form-control" placeholder="********" v-model="usuario.senha" required>
+        </div>
+        <button @click="logar()" type="submit" class="btn btn-primary">Login</button>
+        <router-link to="/usuario-register"><p class="message">Ainda não está registrado? <a>Criar conta</a></p></router-link>
+      </form>
     </div>
   </div>
 </template>
@@ -41,69 +38,130 @@ export default {
   methods:{
     async logar(){
       this.error = '';
+      if (!this.usuario.email){
+        alert('Informe o email para logar');
+        return;
+      }
+      if (!this.usuario.senha){
+        alert('Informe a senha do usuário');
+        return;
+      }
       try{
         await login(this.usuario.email, this.usuario.senha);
         this.$router.push('/index');
-       // window.location.reload();
+        //
       }catch(error){
         this.error = error.response.data.message;
+        alert(this.error);
+        window.location.reload();
       }
-    }
+    },
   }
 }
 </script>
 
 <style scoped>
-  form{
-    flex-direction: column;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+  @import url(https://fonts.googleapis.com/css?family=Roboto:300);
 
-  /*/input{
-    margin-top: 10px;
-    border-radius: 10px;
-    background-color: #504B43;
-    color: #fff;
-    border: none;
-    text-align: center;
-    width: 25%;
-  }*/
-
-  p{
-    font-weight: bold;
-    margin-top: 20px;
-    font-size: 20px;
-  }
-
-  /*.botao{
-    width: 120px;
-    border-radius: 10px;
-    color: #fff;
-    font-weight: bold;
-    background-color: #504B43;
-  }*/
-
-  a{
-    text-align: center;
-    text-decoration: none;
-    color: #8FD694;
-    font-size: 12px;
-  }
-  
-  h2{
-    color: #8FD694;
-    font-weight: bold;
-    margin-bottom: 10px;
-    text-align: center;
-    font-size: 45px;
-  }
-  span{
-    margin: 10px;
-  }
-  button{
-    margin-top: 30px;
-    /*justify-content: center;*/
-  }
+.login-page {
+  padding: 8% 0 0;
+  margin: auto;
+}
+.form {
+  position: relative;
+  z-index: 1;
+  background: #FFFFFF;
+  max-width: 460px;
+  margin: 0 auto 100px;
+  padding: 30px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+  margin: auto;
+}
+.label {
+  text-align: left;
+  font-family: "Roboto", sans-serif;
+  font-size: 14px;
+}
+.form input {
+  font-family: "Roboto", sans-serif;
+  outline: 0;
+  background: #f2f2f2;
+  width: 100%;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;
+}
+.form button {
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #4CAF50;
+  width: 100%;
+  border: 0;
+  padding: 15px;
+  color: #FFFFFF;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+.form button:hover,.form button:active,.form button:focus {
+  background: #43A047;
+}
+.form .message {
+  margin: 15px 0 0;
+  color: #b3b3b3;
+  font-size: 12px;
+}
+.form .message a {
+  color: #4CAF50;
+  text-decoration: none;
+}
+.form .register-form {
+  display: none;
+}
+.container {
+  position: relative;
+  z-index: 1;
+  max-width: 300px;
+  margin: 0 auto;
+}
+.container:before, .container:after {
+  content: "";
+  display: block;
+  clear: both;
+}
+.container .info {
+  margin: 50px auto;
+  text-align: center;
+}
+.container .info h1 {
+  margin: 0 0 15px;
+  padding: 0;
+  font-size: 36px;
+  font-weight: 300;
+  color: #1a1a1a;
+}
+.container .info span {
+  color: #4d4d4d;
+  font-size: 12px;
+}
+.container .info span a {
+  color: #000000;
+  text-decoration: none;
+}
+.container .info span .fa {
+  color: #EF3B3A;
+}
+body {
+  background: #76b852; /* fallback for old browsers */
+  background: rgb(141,194,111);
+  background: linear-gradient(90deg, rgba(141,194,111,1) 0%, rgba(118,184,82,1) 50%);
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;      
+}
 </style>
