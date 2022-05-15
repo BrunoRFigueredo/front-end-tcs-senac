dcc08b003c4095b8c04ce78c2018814a37ea1f37
BETHA\bruno.figueredo <bruno.figueredo@betha.com.br>
2022-05-15 15:24:26 -0300

    Criando upload de imagem para o usuário(voluntário/instituição)

--- src/services/crud.js
+++ src/services/crud.js
@@ -1,29 +1,35 @@
-import { getClient } from './http'
+import {getClient} from './http'
 
 class CrudService {
 
-  constructor(path) {
-    this.path = path
-  }
-
-  findAll({ paginaDesejada=0, tamanhoPagina=10, filter='' }) {
-    return getClient().get(`${this.path}?paginaDesejada=${paginaDesejada}&tamanhoPagina=${tamanhoPagina}&filter=${filter}`)
-  }
-
-  findById(id) {
-    return getClient().get(`${this.path}${id}`)
-  }
-
-  save(record) {
-    if (record.id) {
-      return getClient().put(`${this.path}${record.id}`, record)
+    constructor(path) {
+        this.path = path
     }
-    return getClient().post(this.path, record)
-  }
 
-  remove(id) {
-    return getClient().delete(`${this.path}${id}`)
-  }
+    findAll({paginaDesejada = 0, tamanhoPagina = 10, filter = ''}) {
+        return getClient().get(`${this.path}?paginaDesejada=${paginaDesejada}&tamanhoPagina=${tamanhoPagina}&filter=${filter}`)
+    }
+
+    findById(id) {
+        return getClient().get(`${this.path}${id}`)
+    }
+
+    save(record) {
+        if (record.id) {
+            return getClient().put(`${this.path}${record.id}`, record)
+        }
+        return getClient().post(this.path, record)
+    }
+
+    saveImagem(imagem) {
+        const formData = new FormData();
+        formData.append('file', imagem);
+        return getClient().post(this.path, formData)
+    }
+
+    remove(id) {
+        return getClient().delete(`${this.path}${id}`)
+    }
 }
 
 export default CrudService
--- src/views/register/Form.vue
+++ src/views/register/Form.vue
@@ -1,36 +1,44 @@
 <template>
   <!-- <dir id="user-form"> -->
-    <form action="" class="col-md-8" id="formulario" @submit="checkForm">
-      <h3 class="text-center pb-4"><strong>Cadastrar usuário</strong></h3>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Seu nome</label>
-        <input type="text" class="form-control" v-model="usuario.nome" required>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email</label>
-        <input type="email" class="form-control" v-model="usuario.email" required>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Senha</label>
-        <input type="password" class="form-control" v-model="usuario.senha" required>
-      </div>
-      <label for="exampleInputEmail1" class="form-label">Confirmar senha</label>
-      <div class="mb-3">
-        <input type="password" class="form-control" v-model="usuario.confirmarSenha" required>
-      </div>
-      <div class="col-md-12">
-        <button type="submit" class="btn btn-success" :disabled="desabilitar()" @click="cadastrar">Cadastrar</button>
-      </div>
-    </form>
+  <form action="" class="col-md-8" id="formulario" @submit="checkForm">
+    <h3 class="text-center pb-4"><strong>Cadastrar usuário</strong></h3>
+    <div class="mb-12" >
+    <img id="blah" :src="url" alt="your image" width="200" height="200" style="align-content: center" />
+    </div>
+    <div class="mb-3">
+      <input accept="image/*" type="file" class="form-control" @change="onFileChange" id="customFile"/>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Seu nome</label>
+      <input type="text" class="form-control" v-model="usuario.nome" required>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email</label>
+      <input type="email" class="form-control" v-model="usuario.email" required>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Senha</label>
+      <input type="password" class="form-control" v-model="usuario.senha" required>
+    </div>
+    <label for="exampleInputEmail1" class="form-label">Confirmar senha</label>
+    <div class="mb-3">
+      <input type="password" class="form-control" v-model="usuario.confirmarSenha" required>
+    </div>
+    <div class="col-md-12">
+      <button type="submit" class="btn btn-success" :disabled="desabilitar()" @click="cadastrar">Cadastrar</button>
+    </div>
+  </form>
   <!-- </dir> -->
 </template>
 
 <script>
-import api from "../../services/api";
+import CrudService from "@/services/crud";
+
 export default {
-  name: "user-form",
-  data(){
-    return{
+  name: "form-usuario",
+  data() {
+    return {
+      url: null,
       usuario: {
         nome: '',
         email: '',
@@ -38,36 +46,54 @@ export default {
         confirmarSenha: '',
         dataHoraCadastro: Date.now(),
         status: 1,
-        perfilPermissao: 1
+        perfilPermissao: 1,
+        imagem: ''
       }
     }
   },
+  mounted() {
+    this.$crudUsuario = new CrudService('/usuario/');
+    this.$crudImagem = new CrudService('/imagem/');
+  },
   methods: {
-    cadastrar(){
-      if (this.usuario.senha != this.usuario.confirmarSenha){
+    async cadastrar() {
+      if (this.usuario.senha != this.usuario.confirmarSenha) {
         alert('A senha não confere com a confirmaçao');
       } else {
-        api.post("/usuario/criar-usuario", this.usuario)
-        .then((r) => {
-            alert('Usuário criado com sucesso');
-            this.$router.push('/usuario-login');
+        if (this.files) {
+          const imagem = await this.$crudImagem.saveImagem(this.files);
+          console.log(imagem);
+          this.usuario.imagem = imagem.data.idImagem;
+        }
+        this.$crudUsuario.save(this.usuario).then((r) => {
+          alert('Usuário criado com sucesso');
+          this.$router.push('/usuario-login');
         }).catch(error => {
-            alert('Erro ao criar usuário ' + error.response.data.message);
+          alert('Erro ao criar usuário ' + error.response.data.message);
         })
       }
     },
-    desabilitar(){
+    desabilitar() {
       return (!this.usuario.nome || this.usuario.nome.length > 100) ||
-             (!this.usuario.email || this.usuario.email.length > 150) ||
-             (!this.usuario.senha || this.usuario.senha.length > 100) ||
-             (!this.usuario.confirmarSenha || this.usuario.confirmarSenha.length > 100);
+          (!this.usuario.email || this.usuario.email.length > 150) ||
+          (!this.usuario.senha || this.usuario.senha.length > 100) ||
+          (!this.usuario.confirmarSenha || this.usuario.confirmarSenha.length > 100);
+    },
+    onFileChange(e) {
+      const file = e.target.files[0];
+      this.url = URL.createObjectURL(file);
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) {
+        return;
+      }
+      this.files = files[0];
     }
   },
 }
 </script>
 
 <style scoped>
-  #formulario{
-    margin: 0 auto;
-  }
+#formulario {
+  margin: 0 auto;
+}
 </style>
