3e172b6dc28a4103bb4e02f630e525cbecec255e
colombolucas <colombo.lucasit@gmail.com>
2022-05-20 12:13:23 -0300

    Merge branch 'main' of https://github.com/BrunoRFigueredo/front-end-tcs-senac

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <nav>
+    <div class="link">
+      <router-link to="/">Início</router-link> | 
+      <router-link to="/users">Voluntários</router-link> |
+      <router-link to="/instituition">Instituições</router-link> |
+      <router-link to="/project">Projetos</router-link> |
+    </div>
+    <div class="usuario">
+      <div><strong style="color: #504B43">{{user.nome}}</strong></div>
+      <div>
+        <router-link to="/#" class="text-usuario" v-if="!user.nome" @click="logar()">Acessar conta</router-link> | 
+        <router-link to="/user-register" class="text-usuario" v-if="!user.nome">Cadastrar</router-link>
+        <button @click="deslogar()" v-if="user.nome" class="botaoDesconectar">Desconectar</button>
+      </div>
+    </div>
+  </nav>
+  <router-view class="mt-5 "/>
+</template>
+
+<script>
+export default {
+    data(){
+      return{
+        user: {
+          id: 1,
+          nome: "Felipe",
+          email: "felipe-daros@hotmail.com",
+          password: '123',
+          confirmPassword: '123',
+          dataHoraCadastro: Date.now(),
+          status: 1,
+          perfilPermissao: 3
+        }
+      }
+    },
+    methods:{
+      logar(){
+        this.user.nome = "Felipe"
+      },
+      deslogar(){
+        this.user.nome = null
+      }
+    }
+}
+</script>
+
+<style scoped>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+nav {
+  width: 100%;
+  padding-left: 2%;
+  padding-right: 2%;
+  height:80px;
+  padding-top: 20px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #77AD78	;
+  text-decoration: none;
+}
+
+nav a.router-link-exact-active {
+  color: #7DBA84	;
+  border-bottom: 1px solid #7DBA84	;
+}
+
+nav a:hover{
+  color: #8FD694	;
+}
+
+.usuario{
+  float: right;
+}
+
+.link{
+  float: left;
+}
+
+.text-usuario{
+  font-size: 10px;
+  text-align: center;
+  padding-top: 5%;
+  color: red;
+}
+
+.botaoDesconectar{
+  border-radius: 10px;
+  color: #fff;
+  font-weight: bold;
+  background-color: #504B43;
+  text-decoration: none;
+  font-weight: bold;
+  padding: 5px;
+  font-size: 10px;
+}
+</style>
---
+++ src/views/project/ProjectIndex.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="index-instituition">
+    <div class="container">
+      <div class="row">
+        <div class="card" style="width: 18rem;" v-for="inst in projects" :key="inst.id">
+          <img src="https://pokeapi.co/api/v2/ability/7/" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">{{inst.name}}</h5>
+            <p class="card-text">{{inst.cnpj}}</p>
+            <p class="card-text">{{inst.pix}}</p>
+            <router-link to="/home"><a href="#" class="btn btn-primary">Visualizar</a></router-link>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import api from "../../services/api";
+export default {
+  data(){
+    return{
+      projects: [],
+    }
+  },
+  mounted(){
+    api.get("/projects").then(r => {
+      this.projects = r.data;
+      console.log(this.projects);
+    })
+  }
+}
+</script>
+
+<style scoped>
+  .botao{
+    width: 150px;
+    border-radius: 10px;
+    color: #fff;
+    font-weight: bold;
+    background-color: #504B43;
+  }
+
+  .card{
+    margin: 10px auto;
+  }
+</style>
