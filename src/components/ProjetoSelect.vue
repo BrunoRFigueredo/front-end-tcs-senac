ddb267dc9bd673dcc049b42b9f4165d3bae32115
FelipeDaros <felipe-daros@hotmail.com>
2022-05-25 22:42:03 -0300

    Além de adicionar o card novo foi adiconado já o direcionamento do id

---
+++ src/components/ProjetoSelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="projeto-select" class="container">
+    
+  </div>
+</template>
+
+<script>
+import CrudService from '@/services/crud';
+export default {
+  name: 'projeto-select',
+  props: ['id'],
+  data(){
+    return{
+      projeto: {}
+    }
+  },
+  mounted() {
+    this.$crudProjetos = new CrudService('/projeto/');
+    this.carregarProjeto(this.id);
+  },
+  methods: {
+    async carregarProjeto(id){
+      const {data} = await this.$crudProjetos.findById(id);
+      this.projeto = data;
+      console.log(data);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
