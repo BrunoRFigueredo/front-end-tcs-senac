a4f459fcda2de4177251c93070d1b8ff756cf20e
FelipeDaros <felipe-daros@hotmail.com>
2022-06-05 15:10:18 -0300

    alteração projetos

--- src/views/projeto/Index.vue
+++ src/views/projeto/Index.vue
@@ -1,24 +1,19 @@
 <template>
   <div id="index-project" class="container">
-    <div class="row">
-      <div class="card col m-4" v-for="proj in projetos" :key="proj.id">
-        <div class="imgBx">
-          <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34iEOaXace3VrMFdmWGZlPt_tA45DhQgVUA&usqp=CAU">
-        </div>
-        <div class="contentBx">
-          <h2>{{ proj.nome }}</h2>
-          <div class="size mt-4">
-            <h3>Iniciou: {{ proj.data_inicio }}</h3>
-          </div>
-          <div class="color">
-            <h3>Teminará: {{ proj.data_final }}</h3>
-          </div>
+    <div class="row mt-5">
+      <div class="col">
+        <div class="card" v-for="proj in projetos" :key="proj.id">
+        <img src="https://i.postimg.cc/1X8R7m8y/design.png"/>
+        <div class="card__content">
+          <h2>{{proj.nome}}</h2>
+          <p>Início: {{proj.data_inicio}}</p>
+          <p>Início: {{proj.data_final}}</p>
           <router-link :to="`/projeto/${proj.id}`" class="mt-2">Visualizar</router-link>
         </div>
       </div>
+      </div>
     </div>
-    <div class="row">
+    <div class="row mt-5">
       <div class="h5 col-md">
         Página {{ paginaDesejada }} de {{ totalPagina }}
       </div>
@@ -33,16 +28,16 @@
             @update:modelValue="carregarProjetos"
         />
       </div>
-    </div>
-  </div>
-
-  <!-- botões apenas para testes, manter mas adequar conforme novo layout da pagina -->
-
-  <router-link to="/cadastrar-projeto" class="p-5">
+      <router-link to="/cadastrar-projeto" class="p-5">
     <button>
       Cadastrar Projeto - Teste
     </button>
   </router-link>
+    </div>
+  </div>
+  
+    
+    
 
 
 </template>
@@ -86,201 +81,84 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
-
-* {
-  font-family: 'Poppins', sans-serif;
-}
-
-body {
+body{
+  margin: 0 auto;
   display: flex;
   justify-content: center;
   align-items: center;
-  min-height: 100vh;
-  background: #131313;
 }
 
-.container {
-  position: relative;
-}
-
-.container .card {
-  position: relative;
-  width: 320px;
-  height: 450px;
-  background: rgb(33, 37, 41);
-  border-radius: 20px;
-  overflow: hidden;
-}
-
-.container .card:before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: #f4f4f4;
-  clip-path: circle(150px at 80% 20%);
-  transition: 0.5s ease-in-out;
-}
-
-.container .card:hover:before {
-  clip-path: circle(300px at 80% -20%);
-}
-
-/*.container .card:after{
-  content: 'Projeto';
-  position: relative;
-  top: 30%;
-  left: -20%;
-  font-size: 4em;
-  font-weight: 800; 
-  margin-left: 30%;
-  color: #f4f4f4
-}*/
-
-.container .card .imgBx {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  z-index: 10000;
-  width: 100%;
-  height: 220px;
-  transition: 0.5s;
-}
-
-.container .card:hover .imgBx {
-  top: 0%;
-  transform: translateY(0%);
-
-}
-
-.container .card .imgBx img {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 250px;
-  border-radius: 50%;
-}
-
-.container .card .contentBx {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 100px;
-  text-align: center;
-  transition: 1s;
-  z-index: 10;
-}
-
-.container .card:hover .contentBx {
-  height: 210px;
-}
-
-.container .card .contentBx h2 {
-  position: relative;
-  font-weight: 600;
-  letter-spacing: 1px;
-  color: #fff;
-  margin: 0;
-}
-
-.container .card .contentBx .size, .container .card .contentBx .color {
+div.col{
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 8px 20px;
-  transition: 0.5s;
-  opacity: 0;
-  visibility: hidden;
-  padding-top: 0;
-  padding-bottom: 0;
 }
 
-.container .card:hover .contentBx .size {
-  opacity: 1;
-  visibility: visible;
-  transition-delay: 0.5s;
+.card{
+  min-height:200px;
+  width:260px;
+  margin: 10px;
+  padding:1rem;
+  background-color:#fff;
+  float: right;
 }
-
-.container .card:hover .contentBx .color {
-  opacity: 1;
-  visibility: visible;
-  transition-delay: 0.6s;
+.card img{
+  width:100%;
+  height:160px;
+  border-radius:10px;
+  position:relative;
+  z-index:1000;
+  transition:all .5s ease-in-out;
 }
-
-.container .card .contentBx .size h3, .container .card .contentBx .color h3 {
-  color: #fff;
-  font-weight: 300;
-  font-size: 14px;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  margin-right: 10px;
+.card__content{
+  margin:1rem 0;
+  color:#222;
+  overflow:hidden;
+  margin-top:-200px;
+  opacity:0;
+  visibility:hidden;
+  transition:all .5s ease-in-out;
 }
-
-.container .card .contentBx .size span {
-  width: 26px;
-  height: 26px;
-  text-align: center;
-  line-height: 26px;
-  font-size: 14px;
-  display: inline-block;
-  color: #111;
-  background: #fff;
-  margin: 0 5px;
-  transition: 0.5s;
-  color: #111;
-  border-radius: 4px;
-  cursor: pointer;
+.card__content h2{
+  margin:.6rem auto;
+  text-align:center;
 }
-
-.container .card .contentBx .size span:hover {
-  background: #9bdc28;
+.card__content p{
+  font-size:1.1rem;
+  line-height:1.8rem;
+  text-align:center;
 }
-
-.container .card .contentBx .color span {
-  width: 20px;
-  height: 20px;
-  background: #ff0;
-  border-radius: 50%;
-  margin: 0 5px;
-  cursor: pointer;
+.card__content a{
+  width:200px;
+  padding:10px 15px;
+  display:block;
+  text-align:center;
+  margin:.6rem auto;
+  font-size:1.1rem;
+  color:rgb(156, 156, 156);
+  text-decoration:none;
+  background-color: #198754;
+  border-radius:10px;
+  transition:.3s;
 }
-
-.container .card .contentBx .color span:nth-child(2) {
-  background: #9bdc28;
+.card__content a:hover{
+  color:#fff;
+  background-color: #198754;
+  box-shadow:0 0 2px 2px rgba(0,0,0,0.4);
 }
-
-.container .card .contentBx .color span:nth-child(3) {
-  background: #03a9f4;
+.card:hover img{
+  margin-top:-60px;
+  box-shadow:0 0 4px 6px rgba(0,0,0,0.3);
 }
-
-.container .card .contentBx .color span:nth-child(4) {
-  background: #e91e63;
+.card:hover>.card__content{
+  margin-top:0;
+  opacity:1;
+  visibility:visible;
 }
-
-.container .card .contentBx a {
-  display: inline-block;
-  padding: 10px 20px;
-  background: #fff;
-  border-radius: 4px;
-  margin-top: 10px;
-  text-decoration: none;
-  font-weight: 600;
-  color: #111;
-  opacity: 0;
-  transform: translateY(50px);
-  transition: 0.5s;
-  margin-top: 0;
-}
-
-.container .card:hover .contentBx a {
-  opacity: 1;
-  transform: translateY(0px);
-  transition-delay: 0.75s;
-
+@media screen and (max-width:800px){
+  .container{
+    flex-wrap:wrap;
+  }
 }
 </style>
