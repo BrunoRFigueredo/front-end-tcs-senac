8bd597b8d5c14048bdd347e9a44fd263160c244c
Pedro Lucas <pedrolucas.7@hotmail.com>
2022-06-21 20:08:18 -0300

    Ajustes bootstrap

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import router from './router'
 import VueModal from '@kouts/vue-modal'
 import '@kouts/vue-modal/dist/vue-modal.css'
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
-createApp(App).use(router, VueModal).mount('#app')
+createApp(App).use(router, VueModal, BootstrapVue3).mount('#app')
